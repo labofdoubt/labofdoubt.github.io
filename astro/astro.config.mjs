b29320e1c2f779a32a143e4fbf7480b37bd9b09e
Sergey Alekseev <cutethanks@gmail.com>
2025-12-25 20:07:19 +0300

    Added references

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -4,6 +4,7 @@ import sitemap from "@astrojs/sitemap";
 
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
+import rehypeCitation from "rehype-citation";
 
 // NOTE:
 // - For `labofdoubt.github.io` (user/organization pages), `base` should be "/".
@@ -15,7 +16,18 @@ export default defineConfig({
   markdown: {
     remarkPlugins: [remarkMath],
     // Render KaTeX as HTML only (prevents “double rendering” in some browsers)
-    rehypePlugins: [[rehypeKatex, { output: "html" }]],
+    rehypePlugins: [
+      [rehypeKatex, { output: "html" }],
+      [
+        rehypeCitation,
+        {
+          // Put BibTeX entries here:
+          bibliography: ["./src/content/references.bib"],
+          // Rendered bibliography will appear where you place: <div id="refs"></div>
+          linkCitations: true
+        },
+      ],
+    ],
   },
 });
 
